8687e72e6deeac125ade2fdffaa0c19dd07c7855
Romain Dorgueil <hartym@users.noreply.github.com>
2017-10-05 11:50:34 +0200

    Update main.py

--- {{cookiecutter.name}}/main.py
+++ {{cookiecutter.name}}/main.py
@@ -1,15 +1,10 @@
 import bonobo
 
-def get_graph():
-    graph = bonobo.Graph(
-        range(42),
-        bonobo.Filter(filter=lambda x: x % 2),
-        print,
-    )
-
-    return graph
-
-graph = get_graph()
+graph = bonobo.Graph(
+    range(42),
+    bonobo.Filter(filter=lambda x: x % 2),
+    print,
+)
 
 if __name__ == '__main__':
     from bonobo.commands.run import get_default_services
